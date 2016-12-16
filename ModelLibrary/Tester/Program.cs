2759430b62deee682008ea6813b082c1b26bf118
mmatthew <takeknock@gmail.com>
2016-12-16 23:48:17 +0900

    added Tester

---
+++ ModelLibrary/Tester/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLibrary;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SABR model = new SABR();
+            double strike = 100.0;
+            double maturity = 1.0;
+            double interestRate = 0.2;
+
+            double alpha = 0.1;
+            double beta = 0.02;
+            double nu = 0.1;
+            double rho = 0.5;
+
+            double actual = model.calculateHaganLogNormalApproxVol(strike, maturity, interestRate, alpha, beta, nu, rho);
+            Console.WriteLine(actual);
+        }
+    }
+}
