69d898c4942e2628ddf92495ff8fc4d52582f3fe
mmatthew <takeknock@gmail.com>
2016-12-23 22:02:01 +0900

    added Numerical

---
+++ ModelLibrary/Numerical/LUFactorization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Numerical
+{
+    public class LUFactorization
+    {
+        //Matrix<double> mat = new Matrix<double>();
+    }
+}
