f5f2c0d3b701c02fc4b3fd4a3a8f53305b6b6f30
mmatthew <takeknock@gmail.com>
2016-12-28 22:57:31 +0900

    added UtilityTest and RectangleApproximationTest

---
+++ ModelLibrary/UtilityTest/IntegrationTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Utility;
+
+namespace UtilityTest
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        private double testFunction(double x)
+        {
+            return x * x * x + x * x + 1;
+        }
+
+        private double tolerance = 10E-10;
+
+        [Test]
+        public void testRectangleApproximation()
+        {
+            Utility.Integration.RectangleApproximation i = new Utility.Integration.RectangleApproximation();
+            double x = 5;
+
+            // integrate between 0 and 5
+            double expected = x * x * x * x / 4.0 + x * x * x / 3.0 + x;
+
+            double actual = i.integrate(0, 5, 1000, testFunction);
+
+            Assert.AreEqual(expected, actual, tolerance);
+        }
+
+    }
+}
