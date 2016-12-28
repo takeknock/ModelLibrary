0dd4447de445a50f08d39331ee19c3ed38bbcf74
mmatthew <takeknock@gmail.com>
2016-12-28 22:56:29 +0900

    added SimpsonApproximation

---
+++ ModelLibrary/Utility/Integration/SimpsonApproximation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Integration
+{
+    public class SimpsonApproximation
+    {
+        public delegate double integratedFunction(double x);
+
+        public double integrate(double start, double end, int numberOfPartition, Func<double, double> f)
+        {
+            double width = (end - start) / numberOfPartition;
+            double area = 0.0;
+            double next = start + width;
+
+            for (int i = 0; i < numberOfPartition; ++i)
+            {
+                area += width * (f(start) + 4 * f(start + 0.5 * width) + f(next));
+                start += width;
+                next += width;
+            }
+
+            return area;
+        }
+    }
+}
