0af437f9499cb9aa4c3746bda86fb338a383ce4d
mmatthew <takeknock@gmail.com>
2016-12-30 23:15:31 +0900

    added the function which calculates instantanious volatility with Rebonato equation

--- ModelLibrary/ModelLibrary/Rebonato.cs
+++ ModelLibrary/ModelLibrary/Rebonato.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace ModelLibrary
 {
-    class Rebonato
+    public class Rebonato
     {
+        public double getCapletVolatility(double expiry, double a, double b, double c, double d)
+        {
+
+            double term1 = calculateTerm1(expiry, a, c, d);
+
+            double term2 = calculateTerm2(expiry, a, b, c, d);
+
+            double term3 = calculateTerm3(expiry, b, c);
+
+            double varianceExpiry = calculateVolatility(expiry, c, term1, term2, term3);
+
+            double initial = 0.0;
+
+            double term1ForInitial = calculateTerm1(initial, a, c, d);
+            double term2ForInitial = calculateTerm2(initial, a, b, c, d);
+            double term3ForInitial = calculateTerm3(initial, b, c);
+
+            double varianceInitial = calculateVolatility(initial, c, term1ForInitial, term2ForInitial, term3ForInitial);
+
+            double variance = varianceExpiry - varianceInitial;
+            double volatility = variance / expiry;
+
+            return volatility;
+        }
+
 
         private double calculateTerm1(double expiry, double a, double c, double d)
         {
@@ -17,7 +42,7 @@ namespace ModelLibrary
             return term1;
         }
 
-        private double calculateTerm2(double expiry, double b, double c)
+        private double calculateTerm2(double expiry, double a, double b, double c, double d)
         {
             double term2 = 2.0 * b * c * (2.0 * a * c * expiry + a + 4.0 * d * Math.Exp(c * expiry) * (c * expiry + 1.0));
             return term2;
@@ -29,6 +54,13 @@ namespace ModelLibrary
             return term3;
         }
 
+        private double calculateVolatility(double expiry, double c, double term1, double term2, double term3)
+        {
+            double volatility = 1.0 / (4.0 * c * c * c) * Math.Exp(-c * expiry) * Math.Exp(-c * expiry)
+                * (term1 - term2 + term3);
+
+            return volatility;
+        }
 
     }
 }
