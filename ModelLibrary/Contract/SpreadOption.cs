f980008445154218c255f572e1f570e0ce2b4ee2
mmatthew <takeknock@gmail.com>
2016-12-31 10:22:59 +0900

    added SpreadOption contract

---
+++ ModelLibrary/Contract/SpreadOption.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    class SpreadOption
+    {
+        public double _strike { get; private set; }
+        public double _maturity { get; private set; }
+        public bool _isCall { get; private set; }
+
+        SpreadOption(double strike, double maturity, bool isCall)
+        {
+            _strike = strike;
+            _maturity = maturity;
+            _isCall = isCall;
+        }
+
+        public double payoff(double underlying)
+        {
+            if (_isCall)
+                return Math.Max(underlying - _strike, 0);
+
+            return Math.Max(_strike - underlying, 0);
+
+        }
+
+
+    }
+}
