0b9b020146399a37b9dc59d457a40fd923ba7438
mmatthew <takeknock@gmail.com>
2017-01-08 01:48:59 +0900

    added cash-on-delivery option class

---
+++ ModelLibrary/Contract/CashOnDeliveryOption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    public class CashOnDeliveryOption
+    {
+        public double _maturity { get; private set; }
+        public double _strike { get; private set; }
+        public double? _premium { get; private set; }
+
+        CashOnDeliveryOption(double maturity)
+        {
+            _maturity = maturity;
+        }
+        public double payoff(double underlying)
+        {
+            if (_premium == null)
+                throw new NullReferenceException();
+            return Math.Max(underlying - _strike - (double)_premium, 0);
+        }
+
+    }
+}
