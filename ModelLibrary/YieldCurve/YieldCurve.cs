4c49e5b294cee7d55498c740ce4383642f40183d
mmatthew <takeknock@gmail.com>
2017-01-15 16:28:58 +0900

    added YieldCurve project

---
+++ ModelLibrary/YieldCurve/YieldCurve.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Market;
+
+namespace YieldCurve
+{
+    public class YieldCurve
+    {
+        
+        public YieldCurve(List<SwapRate> swapRate)
+        {
+
+        }
+    }
+}
