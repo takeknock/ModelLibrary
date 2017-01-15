4222a1186430525985845bf5d71a69f085403c88
mmatthew <takeknock@gmail.com>
2017-01-15 16:28:43 +0900

    added Market project and SwapRate class

---
+++ ModelLibrary/Market/SwapRate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market
+{
+    public class SwapRate
+    {
+        public double value;
+
+        public SwapRate(double swapValue)
+        {
+            value = swapValue;
+        }
+
+        public decimal? ToDecimal()
+        {
+            if (value == 0.0)
+                return null;
+
+            return (decimal)value;
+        }
+    }
+}
