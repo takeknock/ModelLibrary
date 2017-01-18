3e037c25897c76dee7476a8e8e1e2e30da3ef851
mmatthew <takeknock@gmail.com>
2017-01-18 22:11:05 +0900

    added swig generated files

--- ModelLibrary/Adapter/Class1.cs
+++ ModelLibrary/Adapter/Class1.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Adapter
 {
     public class Class1
     {
+        static void Main()
+        {
+            YieldCurve yc = new global::YieldCurve();
+            yc.build();
 
+            YieldCurveBuilder builder = new YieldCurveBuilder();
+            yc = builder.createYieldCurve();
+        }        
     }
 }
