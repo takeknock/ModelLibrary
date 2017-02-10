90a02c93074e8a6fd4ba000598505e6b81b95271
mmatthew <takeknock@gmail.com>
2017-02-10 22:09:44 +0900

    Added reversionRate, some member variables and calculateVolatility() for calculating discount bond volatility.

--- ModelLibrary/ModelLibrary/HullWhite.cs
+++ ModelLibrary/ModelLibrary/HullWhite.cs
@@ -8,6 +8,16 @@ namespace ModelLibrary
 {
     class HullWhite
     {
-        public double reversionRate { get; private set; }
+        public double reversionRate
+        {
+            get { return _theta * _a; }
+        }
+        private double _theta;
+        private double _a;
+        private double _shortRateVol;
+        private double calculateVolatility(double tau)
+        {
+            return _shortRateVol / _a * (1.0 - Math.Exp(-_a * tau));
+        }
     }
 }
