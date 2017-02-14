affd062b062c6340a3d813f35f758e0cbef5a0dc
mmatthew <takeknock@gmail.com>
2017-02-15 00:16:38 +0900

    added DualNumber class

---
+++ ModelLibrary/Utility/DualNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+
+    class DualNumber<T>
+    {
+        public T _a { get; private set; }
+        public T _b { get; private set; }
+        private Type this_type = typeof(DualNumber<T>);
+
+        //static public this_type operator +=(DualNumber<T> rhs)
+        //{
+        //    _a += rhs._a;
+        //    return this;
+        //}
+    }
+}
