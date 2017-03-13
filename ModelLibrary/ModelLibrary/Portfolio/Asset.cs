d43500363d09708c860dcb2e8d68c558b45577e1
mmatthew <takeknock@gmail.com>
2017-03-13 23:03:16 +0900

    Added Asset class and EfficientPortfolio class

---
+++ ModelLibrary/ModelLibrary/Portfolio/Asset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.Portfolio
+{
+    class Asset
+    {
+        public double volatility { get; private set; }
+        public double earningRatio { get; private set; }
+        public double expectedEarningRatio { get; private set; }
+        public Asset(double volatility, double earningRatio, double expectedEarningRatio)
+        {
+            this.volatility = volatility;
+            this.earningRatio = earningRatio;
+            this.expectedEarningRatio = expectedEarningRatio;
+        }
+    }
+}
---
+++ ModelLibrary/ModelLibrary/Portfolio/EfficientFrontier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.Portfolio
+{
+    class EfficientFrontier
+    {
+        // 2 asset case
+
+    }
+}
