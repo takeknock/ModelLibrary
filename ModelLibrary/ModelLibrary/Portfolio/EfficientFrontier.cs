47102b4d4485493c4c47aecb49a5bd448200665f
mmatthew <takeknock@gmail.com>
2017-03-13 23:21:01 +0900

    Implemented EfficientFrontier (2 asset version)

--- ModelLibrary/ModelLibrary/Portfolio/EfficientFrontier.cs
+++ ModelLibrary/ModelLibrary/Portfolio/EfficientFrontier.cs
@@ -8,7 +8,42 @@ namespace ModelLibrary.Portfolio
 {
     class EfficientFrontier
     {
+
+        public double weightA { get; private set; }
+        public double weightB { get; private set; }
+        private Asset a;
+        private Asset b;
+        public double volatility { get; private set; }
+        public double correlation { get; private set; }
+        public double covariance { get; private set; }
+        private double tolerance = 10E-9;
+
         // 2 asset case
+        public EfficientFrontier(Asset a, Asset b, double weightA, double covariance)
+        {
+            this.weightA = weightA;
+            this.weightB = 1.0 - weightA;
+            this.a = a;
+            this.b = b;
+            correlation = covariance / (a.volatility * b.volatility);
+            volatility = weightA * weightA * a.volatility * a.volatility
+                + (1.0 - weightA) * (1.0 - weightA) * b.volatility * b.volatility
+                + 2.0 * weightA * (1.0 - weightA) * a.volatility * b.volatility * correlation;
+        }
+        public void chanveWeight(double weightA)
+        {
+            this.weightA = weightA;
+            weightB = 1.0 - weightA;
+            volatility = calculatePortfolioVolatility(weightA, a, b);
+        }
+
+        private double calculatePortfolioVolatility(double weightA, Asset a, Asset b)
+        {
+            return weightA * weightA * a.volatility * a.volatility
+                + (1.0 - weightA) * (1.0 - weightA) * b.volatility * b.volatility
+                + 2.0 * weightA * (1.0 - weightA) * a.volatility * b.volatility * correlation;
+
+        }
 
     }
 }
