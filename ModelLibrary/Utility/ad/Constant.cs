2c779dfb60879676aae6f4d090662689b0d0c89a
mmatthew <takeknock@gmail.com>
2017-04-02 20:47:54 +0900

    Add automatic differentiation class into ad namespace

---
+++ ModelLibrary/Utility/ad/Constant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.ad
+{
+    class Constant : IAutomaticDifferenciation
+    {
+        private double cnst;
+        private double difference;
+
+        Constant(double cnst)
+        {
+            this.cnst = cnst;
+        }
+
+        public IAutomaticDifferenciation Eval(IAutomaticDifferenciation x, IAutomaticDifferenciation y)
+        {
+            return this;
+        }
+
+    }
+}
---
+++ ModelLibrary/Utility/ad/Operator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.ad
+{
+    class Operator : IAutomaticDifferenciation
+    {
+        private string op = "";
+
+        Operator(string op)
+        {
+            this.op = op;
+        }
+
+        public IAutomaticDifferenciation Eval(IAutomaticDifferenciation x, IAutomaticDifferenciation y)
+        {
+            if (op.Equals(""))
+                throw new InvalidOperationException("No operator is set.");
+
+            // TODO: how do i add operators in the structure?
+            //if (op.Equals("+"))
+            //    return x + y;
+            //else if (op.Equals("-"))
+            //    return x - y;
+            //else if (op.Equals("*"))
+            //    return x * y;
+            //else if (op.Equals("/"))
+            //    return x / y;
+
+            return null;
+        }
+
+    }
+}
