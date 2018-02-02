f01590aad4702a2ce5207e834a70b58beb7ef855
mmatthew <takeknock@gmail.com>
2018-02-02 22:50:47 +0900

    add contracts proj

---
+++ ModelLibrary/Contract/Components/Leg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnumConst;
+
+namespace Contract.Components
+{
+    class Leg
+    {
+        private string payCycle;
+        private DayCountConvention dcc;
+        private decimal notional;
+        private Currency ccy;
+
+    }
+}
---
+++ ModelLibrary/Contract/InterestRateSwap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contract.Components;
+using EnumConst;
+
+namespace Contract
+{
+    class InterestRateSwap
+    {
+        private Leg rcvLeg;
+        private Leg payLeg;
+        private DateTime tradeDate;
+        private DateTime effectiveDate;
+        private DiscountType discType;
+
+
+    }
+}
