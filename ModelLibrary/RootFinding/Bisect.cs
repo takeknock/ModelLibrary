35e3761580c746577a1d19a722ebf3299ff34056
mmatthew <takeknock@gmail.com>
2018-05-18 22:19:56 +0900

    add rootfinding proj

---
+++ ModelLibrary/RootFinding/Bisect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootFinding
+{
+    public class Bisect : IRootFinder
+    {
+        private readonly double _initial;
+        private readonly Func<double> _f;
+        private readonly double _epsilon;
+
+        public double Answer { get; private set; }
+
+        public Bisect(Func<double> targetFunction, double initial, double epsilon = 1E-10)
+        {
+            _f = targetFunction;
+        }
+
+        public void solve()
+        {
+        }
+
+
+
+
+    }
+}
---
+++ ModelLibrary/RootFindingTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RootFinding;
+
+namespace RootFindingTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        double quadraticTestFunction(double x)
+        {
+            return x * x + 3.0 * x - 4.0;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // test
+            double x = 1.0;
+            const double expectedRight = 4.0;
+            const double expectedLeft = -1.0;
+
+            Bisect bisect = new Bisect(quadraticTestFunction, 14.0);
+            bisect.solve();
+            double actual = bisect.Answer;
+        }
+    }
+}
